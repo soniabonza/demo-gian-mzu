0c7a1e1d28330e68628d2d39ea019271218ec2ae
soniabonza <soniabonzabongsha@gmail.com>
2023-03-23 15:09:41 +0530

    hello

---
+++ 01-hello-r.R
@@ -0,0 +1,15 @@
+#01-hello-r.R
+my_var <- c(
+	       a='apple', 
+	          b='boy', 
+	          c='cat',
+		     d='desktop'
+		  )
+my_var[3]
+
+my_var['c']
+
+my_var[c(3,2,1,4)]
+
+my_var[c('c','b','a','d')]
+
